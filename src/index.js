3c77033e03305586a79f8a444413bc777a913dee
Niels <niels.reijnders@edu-kw1c.com>
2019-01-18 15:56:25 +0100

    removed wrapwords function 😘

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 export default function wrapLines(container, opentag, closingtag) {
         
-    wrapWords = (str, tmpl) => str.replace(/\S+/g, tmpl || '<n>$&</n>');
-
-    container.innerHTML = (wrapWords(container.textContent)); 
+    container.innerHTML = container.textContent.replace(/\S+/g, '<n>$&</n>');     
 
     var spans = container.children, 
         top = 0,
