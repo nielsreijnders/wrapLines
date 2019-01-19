0428892e94197cb89ed31268c230305908b0eeea
Niels <niels.reijnders@edu-kw1c.com>
2019-01-19 17:52:02 +0100

    Removed useless code 💪🏽

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
+
 export default wrapLines = (container, opentag, closingtag) => {
         
     container.innerHTML = container.textContent.replace(/\S+/g, '<n>$&</n>');     
 
-    var spans = container.children, 
+    var spans = container.children,
         top = 0,
-        tmp = '';
+        tmp = opentag;
+
 
     for (let i = 0; i < spans.length; i++) {
 
         var rect = spans[i].getBoundingClientRect().top;
-        if (top === 0) {
-            top = rect;
-            tmp = opentag;
-        }
 
         if (top < rect) {
             tmp += closingtag + opentag;
             top = rect;
         }
         
-        tmp += spans[i].textContent + ' ';    
+        tmp += spans[i].textContent + ' ';   
     }    
     
     tmp += closingtag;
     
-    container.innerHTML = tmp
+    container.innerHTML = tmp;
 
 }
