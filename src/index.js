c0c9a9cec2e1a1b2c0ce6cc06b4115c3846db16e
Niels <niels.reijnders@edu-kw1c.com>
2019-01-19 20:31:57 +0100

    Removed space

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
-
-export default wrapLines = (container, opentag, closingtag) => {
-        
+export default wrapLines = (container, opentag, closingtag) => {       
     container.innerHTML = container.textContent.replace(/\S+/g, '<n>$&</n>');     
-
     var spans = container.children,
         top = 0,
         tmp = opentag;
-
-
     for (let i = 0; i < spans.length; i++) {
-
         var rect = spans[i].getBoundingClientRect().top;
-
         if (top < rect) {
             tmp += closingtag + opentag;
             top = rect;
-        }
-        
+        } 
         tmp += spans[i].textContent + ' ';   
     }    
-    
     tmp += closingtag;
-    
     container.innerHTML = tmp;
-
 }
