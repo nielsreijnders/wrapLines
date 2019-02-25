d69a61f533300e925fb4e3e8b9080465ec0b4bd7
Niels <niels.reijnders@edu-kw1c.nl>
2019-02-25 14:33:57 +0100

    fixed if statement

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ export default function wrapLines(container, opentag, closingtag) {
     container.innerHTML = container.textContent.replace(/\S+/g, '<n>$&</n>');     
     for (let i = 0; i < spans.length; i++) {
         var rect = spans[i].getBoundingClientRect().top;
-        if (top < rect) {
-            tmp += closingtag + opentag;
-            top = rect;
-        } 
+        if (top < rect) tmp += closingtag + opentag;       
+        top = rect;
         tmp += spans[i].textContent + ' ';   
     } 
     container.innerHTML = tmp += closingtag;
